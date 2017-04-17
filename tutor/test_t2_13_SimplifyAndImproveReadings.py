6f106a49f3ebfaf4f3dcc91f02f81409db022ecb
sisiz <zimmerman.sisi@gmail.com>
2017-04-17 00:29:57 -0500

    locally running

--- tutor/test_t2_13_SimplifyAndImproveReadings.py
+++ tutor/test_t2_13_SimplifyAndImproveReadings.py
@@ -27,9 +27,8 @@ LOCAL_RUN = os.getenv('LOCALRUN', 'false').lower() == 'true'
 TESTS = os.getenv(
     'CASELIST',
     str([
-        14745
-        # 14745, 14746, 85291, 100126, 100127,
-        # 100128
+        14745, 14746, 85291, 100126, 100127,
+        100128
     ])
 )
 
